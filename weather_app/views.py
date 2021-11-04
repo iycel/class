ff16079d9c9550eb4913b8e8727e614bffdd05e2
iycel <ismailycel@gmail.com>
2021-11-04 20:47:19 +0300

    feat : weather api key

--- weather_app/views.py
+++ weather_app/views.py
@@ -1,3 +1,14 @@
+from django.http import response
 from django.shortcuts import render
+import requests
+from decouple import config
 
-# Create your views here.
+def index(request):
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid={}'
+    # url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid={}'.format('a', 'b')  bu şekilde de tanımlanabilir
+    # url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={config('API_KEY')}"
+    # response = requests.get(url) bu şekilde de olabilir
+    city = 'istanbul'
+    response = requests.get(url.format(city, config('API_KEY')))
+    return render(request, 'weather_app/index.html')
+    
